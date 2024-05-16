b42e3fb57bd0b5ce0b29106cca80db1b524f1c16
Kayakaga <baijidevelop@gmail.com>
2024-05-16 18:00:41 +0300

    Attack script with explanations

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -4,11 +4,39 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
-    Collider2D target_hitBox;
+    private bool active = false;
+    private Collider2D attack_hitBox = null;
+    
+    /// <summary>
+    /// when attack is active and hitbox collides with an object with HP script, the object takes damage
+    /// </summary>
     void OnTriggerStay() {
-        if (target_hitBox != null) {
-            // After Cosmo finishes the HP script
-            // target_hitBox.GetComponent<HP>().takeDamage(1);
+        //HP hp = attack_hitBox.GetComponent<HP>();
+        if (active && attack_hitBox != null /*&& hp */) {
+            // hp.takeDamage(1);
         }
+    }
+
+    /// <summary>
+    /// sets the attack to be active
+    /// </summary>
+    public void setActive(){
+        active = true;
+    }
+
+    /// <summary>
+    /// sets the attack to be inactive
+    /// </summary>
+    public void setInactive(){
+        active = false;
+    }
+
+    /// <summary>
+    /// sets the attack to be active for a certain amount of time
+    /// </summary>
+    /// <param name="time"></param>
+    public void pingActive(float time){
+        setActive();
+        Invoke("setInactive", time);
     }
 }
