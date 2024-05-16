4e85dbfd6a0eb5bfd9eccdfb3fdb71f51876d557
Kayakaga <baijidevelop@gmail.com>
2024-05-16 19:18:54 +0300

    regions in Platformer.cs

--- Assets/Scripts/Platformer.cs
+++ Assets/Scripts/Platformer.cs
@@ -6,12 +6,14 @@ using UnityEngine;
 
 public class Platformer : MonoBehaviour
 {
+    #region Variables
     Rigidbody2D rb = null;
 
     [Header("Movement Settings")]
     public float speed;
 
     [Space(10)]
+        #region Jump Settings
 
     [Header("Jump Settings")]
     public float jumpForce;
@@ -27,37 +29,49 @@ public class Platformer : MonoBehaviour
     int additionalJumps;
     
     [Space(10)]
+        #endregion
     
+        #region Dash Settings
     [Header("Dash Settings")]
     public float dash_multiplier;
     public float dash_duration, baseSpeed, dash_delay;
     bool isDashing = false, canDash = true;
-
     [Space(10)]
+        #endregion
+
+        #region Jet Pack Settings
     [Header("Jet Pack Settings")]
     public float jet_power;
     public float jet_multiplier, jet_maxMultiplier;
     private float jet_baseMultiplier;
     private bool jet_activated = false;
     private sbyte jet_fuel = 0;
-    public sbyte getFuelValue() { return jet_fuel; }
     [SerializeField][Range(0,100)]
     private sbyte max_fuel = 100;
-    public sbyte getMaxFuel() { return max_fuel; }
     [SerializeField][Range(0,100)]
     private sbyte min_fuel = 0;
-    public sbyte getMinFuel() { return min_fuel; }
 
     [SerializeField]
     private sbyte jet_fuel_discharge = 2;
     [SerializeField]
     private sbyte jet_fuel_recharge = 1;
+        #endregion
 
 
     private float x_step = 0.0f;
+    #endregion
 
 
-    
+/// <summary>
+/// ////////////////// GETTERS AND SETTERS //////////////////
+/// </summary>
+
+    public sbyte getFuelValue() { return jet_fuel; }
+    public sbyte getMaxFuel() { return max_fuel; }
+    public sbyte getMinFuel() { return min_fuel; }
+
+/////////////////// END OF GETTERS AND SETTERS //////////////////   
+
 
     void Start() {
         rb = GetComponent<Rigidbody2D>();
@@ -106,6 +120,7 @@ public class Platformer : MonoBehaviour
 
     }
 
+    #region Basic Physics Movement
     /// <summary>
     /// Applies horizontal movement
     /// </summary>
@@ -120,7 +135,9 @@ public class Platformer : MonoBehaviour
         rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         additionalJumps--;
     }
+    #endregion
 
+    #region Passive Checks
     /// <summary>
     /// Applies gravity to vertical movement
     /// </summary>
@@ -152,7 +169,9 @@ public class Platformer : MonoBehaviour
             isGrounded = false;
         }
     }
+    #endregion
 
+    #region Dash
     /// <summary>
     /// Checks if the player can dash
     /// </summary>
@@ -188,7 +207,9 @@ public class Platformer : MonoBehaviour
     void DashDelay() {
         canDash = true;
     }
+    #endregion
 
+    #region Jetpack
     /// <summary>
     /// waits 0.1 seconds before charging jetpack
     /// </summary>
@@ -210,6 +231,7 @@ public class Platformer : MonoBehaviour
         if (jet_multiplier < jet_maxMultiplier)
             jet_multiplier += jet_baseMultiplier;
     }
+    #endregion
 
     /// <summary>
     /// Gizmo for ground check
