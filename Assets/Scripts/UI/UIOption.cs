35f8899dce8c1fbfef71cab33d01820b1ea11935
Cosmo <htam@student.42berlin.de>
2024-05-18 17:23:27 +0200

    input system package

--- Assets/Scripts/UI/UIOption.cs
+++ Assets/Scripts/UI/UIOption.cs
@@ -6,15 +6,27 @@ using UnityEngine.UI;
 public class UIOption : MonoBehaviour
 {
     [SerializeField] Button _back;
+    [SerializeField] Slider volumeSlider;
+    // public AudioSource audioSource;
 
     void Start()
     {
         _back.onClick.AddListener(ToMenu);
+
+        // if (audioSource && volumeSlider)
+        // {
+        //     volumeSlider.value = audioSource.volume;
+        //     volumeSlider.onValueChanged.AddListener(SetVolume);
+        // }
     }
 
     private void ToMenu() {
         Manager.Instance.LoadScene(Manager.Scene.MainMenu);
     }
 
-    
+    // void SetVolume(float volume) {
+    //     if (audioSource != null) {
+    //         audioSource.volume = volume;
+    //     }
+    // }
 }
